2c9ac5dd38883ee385400d94b5ba47704d032aa6
Joe Downard <joehdownard@gmail.com>
2023-03-17 19:10:11 +0000

    fix

--- simple_rl/agents/func_approx/dsc/MBSkillGraphPlanner.py
+++ simple_rl/agents/func_approx/dsc/MBSkillGraphPlanner.py
@@ -586,7 +586,7 @@ class SkillGraphPlanner(object):
             chain.set_chain_completed()
 
         if chain.is_chain_completed():
-            image = "ant_maze_middle.png" if self.mdp.env_name == "antmaze-dynamic-middle-wall" else "ant_maze_rightmiddle.png"
+            image = "ant_maze_middle" if self.mdp.env_name == "antmaze-dynamic-middle-wall" else "ant_maze_rightmiddle"
             visualize_graph(self, episode, self.chainer.experiment_name, self.chainer.seed, True, background_img_fname=image)
 
     # -----------------------------–––––––--------------
